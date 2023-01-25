f6d40e8dfea5dbf07956ebd1e7ef90629a99ad2e
Алексей Старченко <Prolex>
2023-01-25 19:07:18 +0300

    задача решена

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,9 +1,10 @@
 ﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.WriteLine("Задайте размер массива по Х");
+Console.WriteLine("Задайте колл-во строк");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте размер массива по Y");
+Console.WriteLine("Задайте колл-во столбцов");
 int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 int[,] arr = new int[x, y];
 Random rand = new Random();
 
@@ -14,18 +15,18 @@ for (int i = 0; i < arr.GetLength(0); i++)
         arr[i, j] = rand.Next(1, 100);
 }
 
-/*for (int i = 0; i < arr.GetLength(0); i++) 
+for (int i = 0; i < arr.GetLength(0); i++) 
 {
     for (int j = 0; j < arr.GetLength(1); j++)
         Console.Write(arr[i,j] + "\t  ");
         Console.WriteLine();
-}*/
+}
 
 
-Console.WriteLine("Введите Х координат");
-int fx = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите Y координат");
+Console.WriteLine("Введите номер столбца, нумерация начинается с нуля");
 int fy = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер строки, нумерация начинается с нуля");
+int fx = Convert.ToInt32(Console.ReadLine());
 if (fx > x && fy > y)
     Console.WriteLine("Введеная вами позиция за пределами размера массива, попробуйте снова");
 else
