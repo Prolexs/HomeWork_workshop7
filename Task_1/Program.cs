f003eec5add1e819b623bc586b745c8858b6464d
Алексей Старченко <Prolex>
2023-01-25 17:58:18 +0300

    задача решена

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//double[,] arr = new double[4, 7];
+Random rand = new Random();
+void PrintArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++) 
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rand.Next(-50,50);
+        }
+    }
+    
+}
+
+
+double[,] arr2d = new double[4, 7];
+FillArray(arr2d);
+PrintArray(arr2d);
